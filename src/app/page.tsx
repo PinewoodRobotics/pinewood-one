12192cd23fbc2348e6a63956e5cec6a0f42ddd94
Adam Xu <contact@adamxu.net>
2025-03-22 14:28:38 -0700

    improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         alt="Pinewood One"
         width="500"
         height="500"
-        className="w-1/2 h-auto hover:scale-105 transition-transform duration-300"
+        className="w-1/2 h-auto hover:scale-105 active:scale-95 transition-transform duration-300 ease-in-out cursor-pointer"
         onClick={logoOnClick}
       />
     </div>
@@ -21,7 +21,7 @@ let clicks = 0;
 function logoOnClick() {
   clicks++;
   // make background a little bit darker
-  document.body.style.backgroundColor = `rgba(0, 0, 0, ${clicks * 0.04})`;
+  document.body.style.backgroundColor = `rgba(0, 0, 0, ${clicks * 0.05})`;
   if (clicks >= 10) {
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
   }
