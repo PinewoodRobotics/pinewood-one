acd37e1c193a05ad5b882851321bd49f1962a36a
Adam Xu <contact@adamxu.net>
2025-03-22 14:53:55 -0700

    better video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Image
-        src="/pwone.png"
-        alt="Pinewood One"
-        width="500"
-        height="500"
-        className="w-1/2 h-auto hover:scale-105 active:scale-95 transition-transform duration-300 ease-in-out cursor-pointer"
-        onClick={logoOnClick}
-      />
+    <div>
+      <div className="flex justify-center items-center h-screen z-10">
+        <Image
+          src="/pwone.png"
+          alt="Pinewood One"
+          width="500"
+          height="500"
+          className="w-1/2 h-auto hover:scale-105 active:scale-95 transition-transform duration-300 ease-in-out cursor-pointer"
+          onClick={logoOnClick}
+        />
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full z-[-1]">
+        <video
+          loop
+          playsInline
+          className="w-full h-full object-cover opacity-0"
+          src="https://cdn.hackclubber.dev/slackcdn/3342a21636498dd66c6e423e694eb841.mp4"
+          id="video"
+        />
+      </div>
     </div>
   );
 }
@@ -20,9 +31,23 @@ let clicks = 0;
 
 function logoOnClick() {
   clicks++;
-  // make background a little bit darker
-  document.body.style.backgroundColor = `rgba(0, 0, 0, ${clicks * 0.05})`;
-  if (clicks >= 10) {
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  document.body.style.backgroundColor = `rgba(0, 0, 0, ${clicks * 0.08})`;
+
+  if (clicks >= 13) {
+    const video = document.getElementById("video") as HTMLVideoElement;
+    if (video) {
+      const playPromise = video.play();
+      video.style.opacity = "100";
+
+      if (playPromise !== undefined) {
+        playPromise
+          .then(() => {
+            console.log("Video playing successfully");
+          })
+          .catch((error) => {
+            console.error("Error playing video:", error);
+          });
+      }
+    }
   }
 }
