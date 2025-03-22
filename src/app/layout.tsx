1430f22f736d417bc4aadb571e45831e0a27fc0f
Adam Xu <contact@adamxu.net>
2025-03-22 11:57:12 -0700

    yes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata: Metadata = {
 };
 
 const inter = Inter({ subsets: ["latin"] });
+const interVariable = inter.style.fontFamily;
 
 export default function RootLayout({
   children,
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body className={`${interVariable} antialiased`}>{children}</body>
     </html>
   );
 }
