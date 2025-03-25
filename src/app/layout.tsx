cc51ca3095779e9069d329513b3c012218f4bfc8
Adam Xu <contact@adamxu.net>
2025-03-25 15:13:39 -0700

    revert to good website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "One",
+  description: "Pinewood One",
+};
 
 const inter = Inter({ subsets: ["latin"] });
 const interVariable = inter.style.fontFamily;
